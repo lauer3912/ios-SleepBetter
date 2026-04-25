6d2a7919790d3fffb87e31e5357b418926b944e7
Developer <dev@ggsheng.com>
2026-04-25 13:07:13 +0800

    Add @MainActor to SettingsViewModel

--- SleepBetter/Sources/ViewModels/SettingsViewModel.swift
+++ SleepBetter/Sources/ViewModels/SettingsViewModel.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Combine
 
+@MainActor
 final class SettingsViewModel: ObservableObject {
     @Published var userProfile: UserProfile
     @Published var sleepGoal: SleepGoal
